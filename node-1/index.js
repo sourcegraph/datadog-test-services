e1b5ab5de141949a6f43d5f5cd96e3f656192741
TJ Kandala <kandalatj@gmail.com>
2021-07-23 19:59:33 -0400

    remove doc comment and axios

--- node-1/index.js
+++ node-1/index.js
@@ -3,20 +3,10 @@ const tracer = require("dd-trace").init({
   env: "prod",
 });
 
-/**
- * command to set up agent:
- *
- * docker run -d --name dd-agent -v /var/run/docker.sock:/var/run/docker.sock:ro -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e DD_API_KEY=<OUR_API_KEY> -e DD_SITE="datadoghq.com" -e DD_APM_ENABLED=true -p 8126:8126/tcp gcr.io/datadoghq/agent:7
- *
- * Basically, make sure to add:
- * -e DD_APM_ENABLED=true
- * -p 8126:8126/tcp
- */
-
 const express = require("express");
 const app = express();
 
-const axios = require("axios");
+const http = require("http");
 
 app.get("/", (req, res) => {
   tracer.trace("home", () => {
@@ -25,8 +15,19 @@ app.get("/", (req, res) => {
 });
 
 app.get("/call-2", async (req, res) => {
-  await axios.get("http://localhost:3001/receive-1");
-  res.send("called service 2");
+  http.get(
+    { hostname: "localhost", port: 3001, method: "GET", path: "/receive-1" },
+    (res2) => {
+      let str = "";
+      res2.on("data", (chunk) => {
+        str += chunk;
+      });
+
+      res2.on("end", () => {
+        res.send(str);
+      });
+    }
+  );
 });
 
 app.listen(3000, () => {
